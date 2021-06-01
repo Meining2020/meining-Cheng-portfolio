39e5967252924cd300ee41c1098098a01c77b132
Meining <meinig.cheng@gmail.com>
2021-06-01 11:46:05 -0400

    first commit added home page

---
+++ scripts.js
@@ -0,0 +1,19 @@
+window.addEventListener('scroll', function () {
+    const navElement = document.querySelector('#nav-bar')
+    if(this.scrollY > 20) {
+        navElement.classList.add('sticky');
+    }else {
+        navElement.classList.remove('sticky')
+    }
+})
+
+
+const burgerIcon = document.querySelector('.fa-bars');
+//function to show nav bar when click burger icon
+burgerIcon.addEventListener('click', function () {
+    const navMenu = document.querySelector('#nav-menu');
+    const navIcon = document.querySelector('#menu-icon');
+    navMenu.classList.toggle('nav-show');
+    navIcon.classList.toggle('fa-times');
+})
+
